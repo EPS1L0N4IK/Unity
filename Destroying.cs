e38aefba8b575b769fd031fd0005749b54da454b
EPS1L0N4IK <strelcov.ilia.2011@gmail.com>
2026-01-21 18:05:29 +0600

    Add files via upload

---
+++ Destroying.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroying : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //уничтожает объект к кторому прикреплен скрипт
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
